b646e97ebbe701c66792b0b5937b1e9342ee6a91
why <triwahyuprasetyo@gmail.com>
2016-07-07 09:47:36 +0700

    add an fixing intent for camera

--- app/src/main/java/com/why/mediasdp/MainActivity.java
+++ app/src/main/java/com/why/mediasdp/MainActivity.java
@@ -1,33 +1,47 @@
 package com.why.mediasdp;
 
 import android.Manifest;
+import android.app.Activity;
 import android.app.AlertDialog;
 import android.content.ComponentName;
+import android.content.ContentResolver;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.pm.ActivityInfo;
 import android.content.pm.PackageManager;
 import android.content.pm.ResolveInfo;
+import android.graphics.Bitmap;
 import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v4.content.FileProvider;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.Toast;
 
+import java.io.File;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.List;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
     private Button buttonWeb, buttonTelp, buttonKamera, buttonMp3, buttonVideo, buttonSMS,
             buttonPeta, buttonActivity, buttonAlertDialog, buttonWhatsApp,
             buttonFacebook, buttonTwitter, buttonExit;
-
+    ImageView ivThumbnailPhoto;
+    Bitmap bitMap;
+    static int TAKE_PICTURE = 1;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -57,6 +71,8 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         buttonTwitter = (Button) findViewById(R.id.buttonTwitter);
         buttonExit = (Button) findViewById(R.id.buttonExit);
 
+        ivThumbnailPhoto = (ImageView) findViewById(R.id.ivThumbnailPhoto);
+
         buttonWeb.setOnClickListener(this);
         buttonTelp.setOnClickListener(this);
         buttonKamera.setOnClickListener(this);
@@ -70,6 +86,20 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         buttonFacebook.setOnClickListener(this);
         buttonTwitter.setOnClickListener(this);
         buttonExit.setOnClickListener(this);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent intent) {
+
+        if (requestCode == TAKE_PICTURE && resultCode== RESULT_OK && intent != null){
+            // get bundle
+            Bundle extras = intent.getExtras();
+
+            // get bitmap
+            bitMap = (Bitmap) extras.get("data");
+            ivThumbnailPhoto.setImageBitmap(bitMap);
+
+        }
     }
 
     @Override
@@ -88,8 +118,13 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                 startActivity(intentTelp);
             }
         } else if (view.getId() == buttonKamera.getId()) {
-            Intent intentKamera = new Intent("android.media.action.IMAGE_CAPTURE");
-            startActivityForResult(intentKamera, 0);
+            //Intent intentKamera = new Intent("android.media.action.IMAGE_CAPTURE");
+            //startActivityForResult(intentKamera, 0);
+// create intent with ACTION_IMAGE_CAPTURE action
+            Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+
+            // start camera activity
+            startActivityForResult(intent, TAKE_PICTURE);
         } else if (view.getId() == buttonMp3.getId()) {
             Intent intent = new Intent(Intent.ACTION_VIEW);
             intent.setDataAndType(Uri
@@ -138,12 +173,19 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                     .setIcon(android.R.drawable.ic_dialog_alert)
                     .show();
         } else if (view.getId() == buttonWhatsApp.getId()) {
+/*
             Intent sendIntent = new Intent();
             sendIntent.setAction(Intent.ACTION_SEND);
             sendIntent.putExtra(Intent.EXTRA_TEXT, "Haloo ini posting dari intent");
             sendIntent.setType("text/plain");
             sendIntent.setPackage("com.whatsapp");
             startActivity(sendIntent);
+*/
+            Uri uri = Uri.parse("smsto:" + "+628995362814");
+            Intent i = new Intent(Intent.ACTION_SENDTO, uri);
+            i.putExtra(Intent.EXTRA_TEXT,  "Haloo ini posting dari intent whatsapp");
+            i.setPackage("com.whatsapp");
+            startActivity(i);
         } else if (view.getId() == buttonFacebook.getId()) {
             Intent shareIntent = new Intent(android.content.Intent.ACTION_SEND);
             shareIntent.setType("text/plain");
